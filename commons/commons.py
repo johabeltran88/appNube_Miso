6f17c9bbb3596d2aeeb237fd95d1c9b3c5b83cb9
Mario Martinez <ma.martinez123@uniandes.edu.co>
2023-11-04 10:03:47 -0500

    Eliminar credenciales del bucket por seguridad

--- commons/commons.py
+++ commons/commons.py
@@ -9,7 +9,7 @@ class Commons:
     @staticmethod
     def init():
         app = Flask(__name__)
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './miso-nube-932084c5cff6.json'
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './miso-nube-credentials.json'
         app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:})^M^JeM?H0+=e(-@35.233.184.37/file-converter'
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         app.config['JWT_SECRET_KEY'] = 'frase-secreta'
